b0d9ccf4d9a8ccc669b7bdc9e8c341d1d73dd37a
Aniket Agrawal <aniket.mpa@gmail.com>
2023-02-14 21:06:20 +0100

    Add initial ideas

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// function to create players
+// -  player name - setter/getter
+// -  player symbol - setter/getter
+
+// function to create game instance
+// - game id - getter
+// - players - getter
+// - game board - getter
+// - initialize game () - initialize all pvt vars
+// - display board()
+// - 
