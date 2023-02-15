8a8a3fcb157d32c0f9f43ca363baeb4e08659248
Aniket Agrawal <agra.aniket@gmail.com>
2023-02-15 21:13:56 +0100

    Add more ideas

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-// function to create players
+// player instance
 // -  player name - setter/getter
 // -  player symbol - setter/getter
 
-// function to create game instance
+// - gameBoard instance
+// - game board array - setter/getter
+// - 
+
+// - game instance
 // - game id - getter
 // - players - getter
-// - game board - getter
+// - game board 
+// - buttons to confirm each player's move - accept last clicked square only
+// - only active for player currently playing
 // - initialize game () - initialize all pvt vars
 // - display board()
-// - 
+// - determine if next move needs to be played () 
+// - display notif if not
+
+// typical flow - click start button, present form for pl1/pl2, pl1 gets first chance to play (by disabling pl2 button)
+// clicks on a box, check if move is legal - if not prompt for re-play, if yes record in game board 
+// check if next move is to be played, if yes disable pl1 button else display notif
+// repeat for pl2 until either someone wins or gameboard is filled up 
