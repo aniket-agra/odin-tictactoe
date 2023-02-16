65064dd0b502173cc2f4d5de436445160a481ebd
Aniket Agrawal <aniket.mpa@gmail.com>
2023-02-16 21:57:11 +0100

    Add more rudimentary ideas

--- js/script.js
+++ js/script.js
@@ -16,6 +16,30 @@
 // - display board()
 // - determine if next move needs to be played () 
 // - display notif if not
+const game = function () {
+  let boardArr = new Array(3);
+  for (let i = 0; i < 3; i++) {
+    boardArr[i] = new Array(3);
+    for (let j = 0; j < 3; j++) {
+      boardArr[i][j] = 0;
+    }    
+  }
+  const getArray = function () {
+
+  }
+  const updateBoard = function (move) {
+    // if legalMove update board and send positive notif 
+    // else negative notif  
+  }
+  const playNext = function () {
+    // check if next move to be played
+  }
+}
+
+// create divs for board game, add listeners to each div
+// create instance of game 
+// for each click call updateBoard with the move - use returned result to either prompt for re-move 
+// or call playNext - if yes, getArray() and display new board. If no, declare winner
 
 // typical flow - click start button, present form for pl1/pl2, pl1 gets first chance to play (by disabling pl2 button)
 // clicks on a box, check if move is legal - if not prompt for re-play, if yes record in game board 
